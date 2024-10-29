ca15960ce0d685f5447be293571a577163e9bfe7
Ibrahim Ahmed <96918810+IbrahimAhmed413@users.noreply.github.com>
2024-10-29 15:40:56 +0500

    feat: corrected the code

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 // const Http = require('http');
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use("/", (req, res, next) => {
+  res.json({ message: "express" });
+  const err = "couldnot access";
+  next(err);
+});
+app.listen(8003, () => {
+  console.log("This is running on http://localhost:8003");
+});
+app.use((req, res, error, next) => {
+  console.log("err:", req);
+});
 
-app.use('/' , (req, res , next)=>{
-    res.json({message:'express'})
-    const err = 'couldnot access';
-    next(err);
-})
-app.listen(8003, ()=>{
-    console.log('This is running on http://localhost:8003');
-})
-
-
-app.use('/' , (req, res)=>{
-    console.log("err: ", req);
-})
-
+//this middleware is not for error handling
+// app.use('/' , (req, res)=>{
+//     console.log("err: ", req);
+// })
+//node.js server creation code
 // Http.createServer((req, res) =>{
 //     res.write("helloworld");
 //     res.end();
@@ -25,4 +27,3 @@ app.use('/' , (req, res)=>{
 // }).listen(7000, ()=>{
 //     console.log('port running on http://localhost:7000')
 // })
-
